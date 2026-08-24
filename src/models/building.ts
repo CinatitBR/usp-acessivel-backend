f91d01b8ed3da7b9b43ffe5446ab8415de38551b
Jessica Miranda Carvalho <carvalhojessica25@outlook.com>
2026-08-23 22:11:51 -0300

    docs: adiciona documentação da rota de prédios no openapi.yaml

--- src/models/building.ts
+++ src/models/building.ts
@@ -10,6 +10,7 @@ export interface Building {
     name: string | null;
     address: string | null;
     phone: string | null;
+    email: string | null;
     website: string | null;
     AccessibilityPoints: AccessibilityPoint[];
 }
