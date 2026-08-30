42ea6aa512f44a32a0bdb2900a241c52424f912e
Jessica Miranda Carvalho <carvalhojessica25@outlook.com>
2026-08-30 11:38:55 -0300

    feat: cria endpoint GET /buildings/{id}/accessibility com tratamento de erros padronizado

--- src/routes/buildings.ts
+++ src/routes/buildings.ts
@@ -1,9 +1,9 @@
 import { Hono } from "hono";
-import {getBuildingsList } from '../services/buildings';
+import {getBuildingsList,getBuildingAccessibilityService } from '../services/buildings';
 
-const buildings = new Hono<{Bindings: {DB: D1Database}}>();
+const buildingRoutes = new Hono<{Bindings: {DB: D1Database}}>();
 
-buildings.get('/', async (c) => {
+buildingRoutes.get('/', async (c) => {
     
     try {
         const db = c.env.DB;
@@ -36,4 +36,58 @@ buildings.get('/', async (c) => {
     }
 
 });
-export default buildings;
+
+
+buildingRoutes.get('/:id/accessibility', async (c) => {
+    const buildingId = c.req.param('id');
+
+    try
+    {
+        const response = await getBuildingAccessibilityService(c.env.DB, buildingId);
+
+        return c.json( {
+            success: true,
+            data: response
+        }, 200);
+
+    } catch(error){
+        const errorMessage = error instanceof Error ? error.message : 'ERR_UNKNOWN';
+        
+        switch(errorMessage){
+            case 'ERR_MISSING_BUILDING_ID':
+                return c.json({
+                    success: false,
+                    error: {
+                        code: 'ERR_MISSING_BUILDING_ID',
+                        message: 'O ID do prédio é obrigatório na URL.'
+                    }
+                }, 400);
+            
+            case 'ERR_BUILDING_NOT_FOUND':
+                return c.json({
+                    success: false,
+                    error: {
+                        code: 'ERR_BUILDING_NOT_FOUND',
+                        message: 'Predio nao encontrado'
+                    }
+
+                }, 404);
+            
+            case 'ERR_FETCH_ACCESSIBILITY_FAILED':
+                default:
+                    return c.json({
+                        success: false,
+                        error: {
+                            code: errorMessage === 'ERR_FETCH_ACCESSIBILITY_FAILED' ? errorMessage: 'ERR_UNKNOWN',
+                            message: 'Erro interno ao processar a requisição.'
+                        }
+                    }, 500);
+
+            
+            
+        }
+    }
+
+})
+
+export default buildingRoutes;
