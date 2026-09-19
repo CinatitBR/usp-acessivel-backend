abe0bc79871ff0fa885b89359bb1a02a82cffab1
cinatit <cinatit.igor.santos@gmail.com>
2026-09-19 16:01:29 -0300

    fix: apply CORS to allow requests from the browser
    Requests from the browser were being blocked because it requires CORS to be applied.

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,25 @@
-import { Hono } from "hono";
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
 import buildingsRoutes from './routes/buildings';
-import visualRoutes from "./routes/visualRoutes";
-import poisRoutes from "./routes/pois";
-import mapReportsRoutes from "./routes/mapReports";
+import visualRoutes from './routes/visualRoutes';
+import poisRoutes from './routes/pois';
+import mapReportsRoutes from './routes/mapReports';
 
 export interface Env {
-    DB: D1Database;
-    BUCKET: R2Bucket;
-
+  DB: D1Database;
+  BUCKET: R2Bucket;
 }
 
-const app = new Hono<{Bindings: Env}>();
+const app = new Hono<{ Bindings: Env }>();
+
+// Apply CORS to all routes, for any origin.
+// In the future, for safety reasons, our frontend domain
+// should be set to be the only origin allowed.
+app.use('*', cors());
 
 app.get('/', (c) => {
-    return c.json({mensagem: 'API RODANDO COM HONO'});
+  return c.json({ mensagem: 'API RODANDO COM HONO' });
 });
-
 
 app.route('/buildings', buildingsRoutes);
 app.route('/visualRoutes', visualRoutes);
